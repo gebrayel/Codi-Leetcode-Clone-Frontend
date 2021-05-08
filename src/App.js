9bc63bbe1fbc29978c6b1f8f692b9e33fb1beeda
rommel sanzonetti <rommel.sanzonetti@gmail.com>
2021-05-07 22:29:06 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
   return (
     <div className="App">
@@ -17,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        
       </header>
     </div>
   );

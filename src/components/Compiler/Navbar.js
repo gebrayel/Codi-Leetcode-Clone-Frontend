295ca19666e72cb3f9f5f08ccc8f8d7a6e5021c5
rommel sanzonetti <rommel.sanzonetti@gmail.com>
2021-05-08 12:24:08 -0400

    clean push firebase Login Google and Github ready

--- src/components/Compiler/Navbar.js
+++ src/components/Compiler/Navbar.js
@@ -1,51 +1,15 @@
 import React, { Component,Fragment } from 'react'
-import Button from '@material-ui/core/Button';
-import {
-            loginWithGoogle,
-            loginWithGithub,
-            GeneralLogOut,
-            providerGithub,
-            providerGoogle,
-            GeneralLoginWithRedirect
-} from '../../config/firebase/firebaseAuth';
 
 
 export const Navbar = () =>{
 
-    const loginGoogle=()=>{
-        loginWithGoogle()
-    }
-
-    const loginGithub=()=>{
-        loginWithGithub()
-    }
     
-    const logOut=()=>{
-        GeneralLogOut()
-    }
-    const redirectLogin=(provider)=>{
-        GeneralLoginWithRedirect(provider)
-    }
 
     return(
         <Fragment>
 
             <h1>Navbar</h1>
-             <Button onClick={() => loginGoogle()} variant="contained" color="primary">
-                Login with google
-            </Button>
-            <Button onClick={() => loginGithub()} variant="contained" color="primary">
-                Login with Github
-            </Button>
-            <Button onClick={() => redirectLogin(providerGoogle)} variant="contained" color="primary">
-                Log in with Google redirect
-            </Button>
-            <Button onClick={() => redirectLogin(providerGithub)} variant="contained" color="primary">
-                Log in with Github redirect
-            </Button>
-            <Button onClick={() => logOut()} variant="contained" color="primary">
-                Log Out
-            </Button>
+             
         </Fragment>
     )
 }

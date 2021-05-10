d840f17eb6abf62e29dd413ee9501c09192bad81
samuelmarina <samuellmarina@gmail.com>
2021-05-10 13:17:53 -0400

    Refactorizar screen difficulties

--- src/screens/Difficulties/Difficulties.js
+++ src/screens/Difficulties/Difficulties.js
@@ -1,33 +1,39 @@
 import React from 'react'
-import animationDat from '../../images/robot_playing.json'
+import kitty from '../../assets/animations/robot_playing.json';
+import '../Difficulties/Difficulties.scss';
+
+import colors from "../../config/colors/colors";
+
+import DifficultyBox from '../../components/DifficultyBox/DifficultyBox';
 import LottieF from '../../components/LottieFile/LottieFile'
-import '../Difficulties/Difficulties.scss'
 
 export default function Difficulties(props) {
     
     return (
-        <div id="bg_diff">
-
-            <div id="navbar_diff">Navbar</div>
-            <div id='kitty2'>
-                <LottieF animationData={animationDat} height={'100%'} width={'50%'} idLF='kitty'/>
+        <div id="background">
+            <div id='lottie'>
+                <LottieF animationData={kitty} height={'100%'} width={'50%'} idLF='kitty'/>
             </div>
-
-            <div id='card1_diff'>
-                <h1 id='h1_diff'>{'<'}Fácil{'>'}</h1>
-                <p id='p1_diff'>Si eres nuevo<br/>programando y<br/>quieres conocer lo<br/>básico de este<br/>mundo.</p>
+            <div id="boxes">
+                <DifficultyBox
+                    title="<Fácil>"
+                    titleColor={colors.easy}
+                >
+                    Si eres nuevo programando y quieres conocer lo básico de este mundo.
+                </DifficultyBox>
+                <DifficultyBox
+                    title="{Intermedio}"
+                    titleColor={colors.medium}
+                >
+                    Si ya llevas un tiempo en este mundo y deseas mejorar tus habilidades.
+                </DifficultyBox>
+                <DifficultyBox
+                    title="#Difícil}"
+                    titleColor={colors.hard}
+                >
+                    ¿Te consideras un programador experto? Entonces vamos a ponerte a prueba.
+                </DifficultyBox>
             </div>
-
-            <div id='card2_diff'>
-                <h1 id='h2_diff'>{'{'}Intermedio{'}'}</h1>
-                <p id='p2_diff'>Si ya llevas un<br/>tiempo en este<br/>mundo y deseas<br/>mejorar tus<br/>habilidades.</p>
-            </div>
-
-            <div id='card3_diff'>
-                <h1 id='h3_diff'>#Difícil</h1>
-                <p id='p3_diff'>¿Te consideras un<br/>programador<br/>experto? Entonces<br/>vamos a ponerte a<br/>prueba.</p>
-            </div>
-        
          </div>
     );
 }

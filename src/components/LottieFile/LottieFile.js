98bf8896c3177f1cd8c01978858170788897b886
pstefy <stefani.perez@correo.unimet.edu.ve>
2021-05-09 21:40:19 -0400

    Componente LottieFile listo

---
+++ src/components/LottieFile/LottieFile.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Lottie from 'react-lottie';
+/*import animationDat from '../../images/robot_playing.json' */
+
+export default function LottieFile(props) {
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: props.animationData,
+        rendererSettings: {
+          preserveAspectRatio: "xMidYMid slice"
+        }
+      };
+    
+    return (
+      <div>
+        <Lottie 
+          options={defaultOptions}
+          height={props.height}
+          width={props.width}
+          id={props.id}
+        />
+      </div>
+    );
+  }
--- src/router/UserRoutes.js
+++ src/router/UserRoutes.js
@@ -2,6 +2,7 @@ import React, { Fragment, useContext } from 'react'
 import { Redirect, Route, Switch } from 'react-router-dom'
 
 import deleteLater from '../deleteLater'
+import actual from '../components/LottieFile/LottieFile'
 
 export const UserRoutes = () => {
 
@@ -12,6 +13,8 @@ export const UserRoutes = () => {
                 <Switch>
 
                     <Route exact path = "/" component = {deleteLater} />
+
+                    <Route exact path = "/actual" component = {actual} />
 
                     <Redirect to = "/"/>
                 </Switch>

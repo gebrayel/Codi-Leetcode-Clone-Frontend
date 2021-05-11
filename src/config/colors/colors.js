0954f5c298a0a7e6179e5300a95c6a044ce51ab2
samuelmarina <samuellmarina@gmail.com>
2021-05-11 15:58:51 -0400

    Agregar colores

--- src/config/colors/colors.js
+++ src/config/colors/colors.js
@@ -1,8 +1,12 @@
 export default {
+    //General
+    background: "#282A36",
     //Difficulty Box
     boxBackground: 'rgba(126, 132, 167, 0.63)',
     boxBackgroundHover: 'rgba(126, 132, 167, 0.829)',
     easy: "#fff",
     medium: "#32EDE9",
     hard: "#F31483",
+    //Difficulty Screen
+    
 }

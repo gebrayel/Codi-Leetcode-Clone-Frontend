1af8efa9a7b40f70d762749beeff9255ac8fff8a
rommel sanzonetti <rommel.sanzonetti@gmail.com>
2021-05-11 19:02:26 -0400

    create payment query method frontend ready

---
+++ src/api/payments/payments.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const endpoint = "/payments";
+
+
+const createPayment=async (paymentInfo)=>{
+    try{
+        const response=await axios.post(endpoint, {
+            payment: paymentInfo,
+        });
+        return response.data;
+        
+    }catch(error){
+        return error;
+    }
+}
+
+
+
+
+
+export default {
+    createPayment,
+}

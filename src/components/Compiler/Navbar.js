b8bd3b680b8adc139193cdb7c319d6503f57ff22
rommel sanzonetti <rommel.sanzonetti@gmail.com>
2021-05-11 10:08:58 -0400

    remove button test queries from backend

--- src/components/Compiler/Navbar.js
+++ src/components/Compiler/Navbar.js
@@ -1,8 +1,19 @@
-import React, { Component } from 'react'
-
+import React, { Component,Fragment } from 'react'
+import Button from '@material-ui/core/Button';
+import fb from '../../config/firebase/firebaseAuth';
+import p from '../../api/problems/problems';
 export const Navbar = () =>{
+
+    const getP=()=>{
+        p.getAllProblems();
+    }
+
     return(
-        <h1>Navbar</h1>
+        <Fragment>
+           
+            <h1>Navbar</h1>
+             
+        </Fragment>
     )
 }
 

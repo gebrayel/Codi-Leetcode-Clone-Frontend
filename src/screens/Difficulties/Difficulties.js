7abbc8e968e2e69cd4ba842bb99a07b1a30781ed
samuelmarina <samuellmarina@gmail.com>
2021-05-11 15:59:46 -0400

    Refactorizar

--- src/screens/Difficulties/Difficulties.js
+++ src/screens/Difficulties/Difficulties.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import kitty from '../../assets/animations/robot_playing.json';
-import '../Difficulties/Difficulties.scss';
+import { makeStyles } from '@material-ui/core/styles';
+import { Box, Grid } from '@material-ui/core';
 
 import colors from "../../config/colors/colors";
 
@@ -8,32 +9,77 @@ import DifficultyBox from '../../components/DifficultyBox/DifficultyBox';
 import LottieF from '../../components/LottieFile/LottieFile'
 
 export default function Difficulties(props) {
-    
+    const classes = useStyles(props);
+
     return (
-        <div id="background">
-            <div id='lottie'>
-                <LottieF animationData={kitty} height={'100%'} width={'50%'} idLF='kitty'/>
-            </div>
-            <div id="boxes">
-                <DifficultyBox
-                    title="<Fácil>"
-                    titleColor={colors.easy}
+        <Box className={classes.container}>
+            <Box
+                className={classes.lottieContainer}
+            >
+                <Box className={classes.lottie}>
+                    <LottieF animationData={kitty}/>
+                </Box>
+            </Box>
+            <Box
+                className={classes.box}
+            >
+                <Grid 
+                    container
+                    className={classes.boxContainer}
                 >
-                    Si eres nuevo programando y quieres conocer lo básico de este mundo.
-                </DifficultyBox>
-                <DifficultyBox
-                    title="{Intermedio}"
-                    titleColor={colors.medium}
-                >
-                    Si ya llevas un tiempo en este mundo y deseas mejorar tus habilidades.
-                </DifficultyBox>
-                <DifficultyBox
-                    title="#Difícil}"
-                    titleColor={colors.hard}
-                >
-                    ¿Te consideras un programador experto? Entonces vamos a ponerte a prueba.
-                </DifficultyBox>
-            </div>
-         </div>
+                    <DifficultyBox
+                        title="<Fácil>"
+                        titleColor={colors.easy}
+                    >
+                        Si eres nuevo programando y quieres conocer lo básico de este mundo.
+                    </DifficultyBox>
+                    <DifficultyBox
+                        title="{Intermedio}"
+                        titleColor={colors.medium}
+                    >
+                        Si ya llevas un tiempo en este mundo y deseas mejorar tus habilidades.
+                    </DifficultyBox>
+                    <DifficultyBox
+                        title="#Difícil"
+                        titleColor={colors.hard}
+                    >
+                        ¿Te consideras un programador experto? Entonces vamos a ponerte a prueba.
+                    </DifficultyBox>
+                </Grid>
+            </Box>
+         </Box>
     );
 }
+
+const useStyles = makeStyles(theme => ({
+    container: {
+        height: "100vh",
+        backgroundColor: colors.background,
+        justifyContent: "center"
+    },
+    lottieContainer: {
+        justifyContent: "center",
+        display: "flex",
+        padding: 30,
+    },
+    lottie: {
+        width: "20%",
+        [theme.breakpoints.down('sm')]: {
+            width: "30%"
+        },
+    },
+    boxContainer: {
+        marginTop: 30,
+        paddingLeft: 100,
+        paddingRight: 100,
+        width: "85%",
+        [theme.breakpoints.down('sm')]: {
+            width: "100%"
+        },
+    },
+    box: {
+        justifyContent: "center",
+        display: "flex"
+    },
+    
+}));

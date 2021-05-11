98f2c4bd2731887cd95f2171952d230c29e44dd9
rommel sanzonetti <rommel.sanzonetti@gmail.com>
2021-05-11 10:19:00 -0400

    return data GetAllproblem query

--- src/api/problems/problems.js
+++ src/api/problems/problems.js
@@ -8,7 +8,8 @@ const getAllProblems= async ()=>{
     try{
         
         const response=await axios.get(endpoint);
-        console.log(response)
+        console.log(response.data)
+        return response.data
 
     }catch(error){
         return error;

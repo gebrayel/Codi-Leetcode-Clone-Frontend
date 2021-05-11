7eb8d8ba44b62279776a29b11b0fde52374b14d3
rommel sanzonetti <rommel.sanzonetti@gmail.com>
2021-05-11 19:03:25 -0400

    remove test button create payment query method

--- src/components/Compiler/Navbar.js
+++ src/components/Compiler/Navbar.js
@@ -1,17 +1,25 @@
 import React, { Component,Fragment } from 'react'
 import Button from '@material-ui/core/Button';
 import fb from '../../config/firebase/firebaseAuth';
-import p from '../../api/problems/problems';
+import p from '../../api/payments/payments';
 export const Navbar = () =>{
 
-    const getP=()=>{
-        p.getAllProblems();
+    const createP=()=>{
+        const payment={
+            date:'2001-02-14 19:10:25-07',
+            amount:70,
+            pm_id:1,
+            user_id:1,
+            active:true,
+            sub_type:1
+        }    
+        p.createPayment(payment);
     }
 
     return(
         <Fragment>
-           
             <h1>Navbar</h1>
+            <Button onClick={()=>createP()}>Create Payment</Button>
              
         </Fragment>
     )

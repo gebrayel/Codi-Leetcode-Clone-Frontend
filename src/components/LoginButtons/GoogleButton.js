64287029d86c6037b2441ee37073673af3e5c78d
samuelmarina <samuellmarina@gmail.com>
2021-05-16 21:47:32 -0400

    Manejar login con Google

--- src/components/LoginButtons/GoogleButton.js
+++ src/components/LoginButtons/GoogleButton.js
@@ -1,13 +1,17 @@
+import { useContext } from "react";
 import { GoogleLoginButton } from "react-social-login-buttons";
 
+import AppContext from "../../helpers/context/context";
+import fb from "../../config/firebase/firebaseAuth";
+
 export const GitHub = () => {
+    const { setUser } = useContext(AppContext);
+
     return (
-        <>
-            <GoogleLoginButton
-                className="LoginButton"
-                onClick={() => alert("Hello")}
-            />
-        </>
+        <GoogleLoginButton
+            className="LoginButton"
+            onClick={() => fb.loginWithGoogle(setUser)}
+        />
     );
 };
 

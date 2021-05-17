ff777068d12fca460c75aedd10f83785834e93e6
samuelmarina <samuellmarina@gmail.com>
2021-05-16 21:51:47 -0400

    Manejar login con GitHub

--- src/components/LoginButtons/GitHubButton.js
+++ src/components/LoginButtons/GitHubButton.js
@@ -1,13 +1,17 @@
+import { useContext } from "react";
 import { GithubLoginButton } from "react-social-login-buttons";
 
+import AppContext from "../../helpers/context/context";
+import fb from "../../config/firebase/firebaseAuth";
+
 export const GitHub = () => {
+    const { setUser }  = useContext(AppContext);
+
     return (
-        <>
-            <GithubLoginButton
-                className="LoginButton"
-                onClick={() => alert("Hello")}
-            />
-        </>
+        <GithubLoginButton
+            className="LoginButton"
+            onClick={() => fb.loginWithGithub(setUser)}
+        />
     );
 };
 

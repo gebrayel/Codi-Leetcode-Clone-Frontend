2237637bde80e63481632ee29e44e5221c65e147
samuelmarina <samuellmarina@gmail.com>
2021-05-16 21:14:50 -0400

    Documentar

--- src/api/payments/payments.js
+++ src/api/payments/payments.js
@@ -2,6 +2,11 @@ import axios from 'axios';
 
 const endpoint = "/payments";
 
+/**
+ * Agregar un pago a la base de datos
+ * @param {JSON} paymentInfo Objeto con información del pago
+ * @returns Array con info del pago
+ */
 const createPayment = async (paymentInfo)=>{
     try {
         const response = await axios.post(endpoint, {

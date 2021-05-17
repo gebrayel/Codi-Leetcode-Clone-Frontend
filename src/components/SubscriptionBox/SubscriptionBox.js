7abd93f70733a538840100914bad6d7c274d9bc5
pstefy <stefani.perez@correo.unimet.edu.ve>
2021-05-17 16:02:55 -0400

    Casi listo SubsBox. Falta tema con los colores

---
+++ src/components/SubscriptionBox/SubscriptionBox.js
@@ -0,0 +1,150 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid } from "@material-ui/core";
+import colors from "../../config/colors/colors";
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Clock from '../../assets/time.png';
+
+export default function SubscriptionBox({ title, amount, time, before, after, ideal, prac, buttonText, color1, color2, color3, ...props }) {
+    const classes = useStyles(props);
+
+    return (
+        <Card className={classes.root}>
+            <CardContent className={classes.contentS1}>
+                <Typography className={classes.titleS}>
+                    {title}
+                </Typography>
+                <Typography className={classes.titleS2}>
+                    {amount}
+                </Typography>
+            </CardContent>
+            <CardContent className={classes.contentS2}>
+                <img src={Clock} alt="Time" className={classes.imgS}/>
+                <Typography className={classes.subtitleS}>
+                    {time}
+                </Typography>
+            </CardContent>
+            <CardContent className={classes.contentS3}>
+                <Typography className={classes.textS}>
+                    {before}
+                </Typography>
+                <Typography className={classes.textS}>
+                    {after}
+                </Typography>
+                <Typography className={classes.textS2}>
+                    {ideal}
+                </Typography>
+                <Typography className={classes.textS3}>
+                    {prac}
+                </Typography>
+            </CardContent>
+            <CardActions className={classes.contentS4}>
+                <Button  className={classes.buttonS}>{buttonText}</Button>
+            </CardActions>
+        </Card>
+    );
+}
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        backgroundColor: (props) => props.color1,
+    },
+    contentS1: {
+        backgroundColor: '#E75656',
+        display:'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    contentS2: {
+        backgroundColor: '#CBCBCD',
+        display:'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    imgS: {
+        maxWidth: '2.5rem',
+        maxHeight: '2.5rem',
+        marginRight: '0.5%',
+        '@media (max-width: 899px)':{
+            maxWidth: '1.5rem',
+            maxHeight: '1.5rem',
+        }
+    },
+    titleS: {
+        fontFamily: 'Roboto',
+        fontStyle: 'normal',
+        fontWeight: 'bold',
+        fontSize: '2rem',
+        color: 'white',
+        marginBottom: '0%',
+    },
+    titleS2: {
+        fontFamily: 'Roboto',
+        fontStyle: 'normal',
+        fontWeight: 'bold',
+        fontSize: '1.4rem',
+        color: 'white',
+    },
+    subtitleS: {
+        fontFamily: 'Roboto',
+        fontStyle: 'normal',
+        fontWeight: 'bold',
+        fontSize: '1.4rem',
+        color: 'black',
+    },
+    contentS3: {
+        paddingTop: '0%',
+    },
+    textS: {
+        textAlign: 'center',
+        fontFamily: 'Roboto',
+        fontStyle: 'normal',
+        fontWeight: 'bold',
+        fontSize: '1.4rem',
+        color: 'white',
+    },
+    textS2: {
+        textAlign: 'center',
+        fontFamily: 'Roboto',
+        fontStyle: 'normal',
+        fontWeight: 'bold',
+        fontSize: '1.4rem',
+        color: 'black',
+        textDecoration: 'underline',
+        paddingTop: '1rem',
+    },
+    textS3: {
+        textAlign: 'center',
+        fontFamily: 'Roboto',
+        fontStyle: 'normal',
+        fontWeight: 'bold',
+        fontSize: '1.4rem',
+        color: 'black',
+    },
+    contentS4: {
+        display: 'flex',
+        justifyContent: 'flex-end',
+    },
+    buttonS: {
+        backgroundColor: '#E75656',
+        color: 'white',
+        textAlign: 'center',
+        fontFamily: 'Roboto',
+        fontStyle: 'normal',
+        fontSize: '1.4rem',
+        textTransform: 'lowercase',
+        textTransform: 'capitalize',
+        borderRadius: '20px',
+        width: '12rem',
+        height: '4rem',
+        "&:hover": {
+            backgroundColor: '#FB2121',
+        }
+    },
+
+
+}));

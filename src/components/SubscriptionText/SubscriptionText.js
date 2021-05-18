ff5ca98e4e9ac8a2533d076df83728ed3b6ae8e5
gebrayel <gebrayel.inatti@correo.unimet.edu.ve>
2021-05-18 02:35:50 -0400

    create substext component

---
+++ src/components/SubscriptionText/SubscriptionText.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import { Box, Grid } from '@material-ui/core';
+
+export default SubscriptionText = () =>{
+
+    return (
+        <>
+            
+        </>
+    )
+}

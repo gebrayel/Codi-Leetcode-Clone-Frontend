d6a9eb75ae7213842ba96b1d29c2e8dd7fb663fc
gebrayel <gebrayel.inatti@correo.unimet.edu.ve>
2021-05-18 03:40:26 -0400

    subsText lottiefile applied

--- src/components/SubscriptionText/SubscriptionText.js
+++ src/components/SubscriptionText/SubscriptionText.js
@@ -1,12 +1,48 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
-import { Box, Grid } from '@material-ui/core';
-
-export default SubscriptionText = () =>{
+import { Box, Grid, Typography } from '@material-ui/core';
+import LottieF from '../LottieFile/LottieFile';
+export default function SubscriptionText ({ type, anim, color, children, textAlign, subColor, ...props }){
+    const classes = useStyles(props);
 
     return (
         <>
-            
+            <Box className={classes.container}>
+                <Box className={classes.lottieContainer}>
+                    <Box className={classes.lottie}>
+                        <LottieF animationData={anim}/>
+                    </Box>
+                </Box>
+                <Box>
+                    <Typography color={color}>
+                        {children}
+                    </Typography>
+                </Box>
+            </Box>
         </>
     )
 }
+const useStyles = makeStyles(theme => ({
+    container: {
+        height: "50px",
+        justifyContent: "center"
+    },
+    lottieContainer: {
+        display: "flex",
+        justifyContent: "center",
+        padding: 30,
+        paddingTop: 100,
+        [theme.breakpoints.down('xs')]: {
+            paddingBottom:0,
+        },
+    },
+    lottie: {
+        width: "100px",
+        [theme.breakpoints.down('sm')]: {
+            width: "30%"
+        },
+        [theme.breakpoints.down('xs')]: {
+            width: "40%"
+        },
+    },
+}));
--- src/screens/Subscription/SubscriptionScreen.js
+++ src/screens/Subscription/SubscriptionScreen.js
@@ -2,9 +2,13 @@ import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import { Box, Grid } from '@material-ui/core';
 import colors from "../../config/colors/colors";
+import lock from '../../assets/animations/lockSubscription.json'
 import SubscriptionBox from '../../components/SubscriptionBox/SubscriptionBox';
+import SubscriptionText from '../../components/SubscriptionText/SubscriptionText'
+import checkM from '../../assets/animations/checkMarkSubscription.json'
+import prem from '../../assets/animations/premiumSubscription.json'
 
-export default function Difficulties(props) {
+export default function SubscriptionScreen(props) {
     const classes = useStyles(props);
 
     return (
@@ -13,6 +17,14 @@ export default function Difficulties(props) {
         <Box className={classes.container}>
             <SubscriptionBox title="<Semanal>" amount="$8/se" time="7 DIAS" before="Antes: $1" after="Ahorro: 0%" ideal="Ideal para:" prac="Practicantes Dummies" buttonText="Subscribirse" color1="#CBCBCD" color2="#E75656" color3="#FB2121"/>
         </Box>
+        <Box className={classes.container}>
+            <SubscriptionText type='1' anim = {lock} color = "#CBCBCD" children='hola soy la pajarita' textAlign='center' subColor='red'/>
+            <SubscriptionText type='1' anim = {checkM} color = "#CBCBCD" children='hola soy la pajarita' textAlign='center' subColor='red'/>
+            <SubscriptionText type='1' anim = {prem} color = "#CBCBCD" children='hola soy la pajarita' textAlign='center' subColor='red'/>
+
+        </Box>
+        
+
         </div>
     );
 }

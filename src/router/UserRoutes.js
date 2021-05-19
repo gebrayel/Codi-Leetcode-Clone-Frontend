7a12224dd13d2f0731ea4697f948f1371472e8ac
andresarl16 <andrucho01@gmail.com>
2021-05-19 06:35:27 -0400

    pull

--- src/router/UserRoutes.js
+++ src/router/UserRoutes.js
@@ -4,6 +4,7 @@ import { Redirect, Route, Switch } from "react-router-dom";
 import deleteLater from "../deleteLater";
 import actual from "../screens/Difficulties/Difficulties";
 import HomeScreen from "../screens/Home/HomeScreen";
+import ProblemsScreen from "../screens/Problems/ProblemsScreen";
 
 export const UserRoutes = () => {
     console.log("Publico");
@@ -16,7 +17,7 @@ export const UserRoutes = () => {
 
                     <Route exact path="/delete" component={deleteLater} />
 
-                    <Route exact path="/actual" component={actual} />
+                    <Route exact path="/actual" component={ProblemsScreen} />
 
                     <Redirect to="/" />
                 </Switch>
--- src/screens/Problems/ProblemsScreen.js
+++ src/screens/Problems/ProblemsScreen.js
@@ -1,43 +1,38 @@
-import React, { Component,Fragment,useState } from 'react'
+import React, { Component, Fragment, useState } from "react";
 
-import ProblemList from '../../components/problemList';
-
+import ProblemList from "../../components/problemList";
 
 const ProblemsScreen = () => {
-    
-    function createData( Numero, Title, Difficulty, Solved) {
-        var color='';
-        if (Numero%2==0){
-            var color="#9399BC"
-        }else{
-            var color="#7E84A7"
+    function createData(Numero, Title, Difficulty, Solved) {
+        var color = "";
+        if (Numero % 2 == 0) {
+            var color = "#9399BC";
+        } else {
+            var color = "#7E84A7";
         }
-        return { Numero, Title, Difficulty, Solved,color };
-        }
+        return { Numero, Title, Difficulty, Solved, color };
+    }
 
     const rows = [
-    createData(1, 'Fibonacci', 'Intermedio', '1'),
-    createData(2, 'Numeros primos', 'Facil', '0'),
-    createData(3, 'Dijkstra', 'Dificil', '1'),
-    createData(4, 'DFS', 'Intermedio', '0'),
-    createData(5, 'Binary Search', 'Intermedio', '1'),
-    createData(6, 'Palindromo', 'Facil', '0'),
-    createData(7, 'Numeros vampiros', 'Intermedio', '1'),
-    createData(8, 'Numeros oblongo', 'Facil', '0'),
-    createData(9, 'Ordenar vector', 'Facil', '1'),
-    createData(10, 'Unir dos vectores', 'Facil', '0')
+        createData(1, "Fibonacci", "Intermedio", "1"),
+        createData(2, "Numeros primos", "Facil", "0"),
+        createData(3, "Dijkstra", "Dificil", "1"),
+        createData(4, "DFS", "Intermedio", "0"),
+        createData(5, "Binary Search", "Intermedio", "1"),
+        createData(6, "Palindromo", "Facil", "0"),
+        createData(7, "Numeros vampiros", "Intermedio", "1"),
+        createData(8, "Numeros oblongo", "Facil", "0"),
+        createData(9, "Ordenar vector", "Facil", "1"),
+        createData(10, "Unir dos vectores", "Facil", "0"),
     ];
-    const title='Problemas'
-    
-    return ( 
-<div>
 
-        <ProblemList title={title} rows={rows}/>
+    const title = "Problemas";
 
-</div>
-        
+    return (
+        <div>
+            <ProblemList title={title} rows={rows} />
+        </div>
+    );
+};
 
-     );
-}
- 
 export default ProblemsScreen;

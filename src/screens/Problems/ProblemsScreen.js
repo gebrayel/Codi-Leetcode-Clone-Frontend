63cdfd3b0fb86708db9914867ed9a3ffca02e1ce
samuelmarina <samuellmarina@gmail.com>
2021-05-27 19:05:22 -0400

    Incluir loader en problems screen

--- src/screens/Problems/ProblemsScreen.js
+++ src/screens/Problems/ProblemsScreen.js
@@ -1,14 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
+import AppContext from "../../helpers/context/context";
 import colors from "../../config/colors/colors";
 import ProblemList from "../../components/ProblemList/ProblemList";
 import problemAPI from "../../api/problems/problems";
+import RobotLoader from "../../components/RobotLoader/RobotLoader";
 import useQuery from "../../hooks/useQuery/useQuery";
 
 const ProblemsScreen = () => {
     const query = useQuery();
     const [problems, setProblems] = useState([]);
     const user = JSON.parse(localStorage.getItem('user'));
+    const { isLoading, setIsLoading } = useContext(AppContext);
 
     const spanishDifficulty = {
         easy: "Fácil",
@@ -19,6 +22,7 @@ const ProblemsScreen = () => {
     useEffect(() => {
         const difficulty = query.get("difficulty");
         const getProblems = async (difficulty, user) => {
+            setIsLoading(true);
             const probs = await problemAPI.getProblemsByDifficulty(difficulty, user);
             let arrayProbs = probs.map((prob, index) => {
                 return { ...prob, 
@@ -27,16 +31,24 @@ const ProblemsScreen = () => {
                 }
             })
             setProblems(arrayProbs);
+            setIsLoading(false);
         };
         getProblems(difficulty, user);
     }, []);
 
     return (
         <div className="ProblemScreenContainer">
+        { isLoading ? 
+        (
+            <RobotLoader />
+        ) :
+        (
             <div className="ProblemListContainer">
                 <h1 className="ProblemScreenTitle"> Problemas </h1>
                 <ProblemList rows={problems} />
             </div>
+        )}
+            
         </div>
     );
 };

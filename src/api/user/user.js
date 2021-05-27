521587e9e525ac4432427c21c2e60414018fe37e
samuelmarina <samuellmarina@gmail.com>
2021-05-27 15:28:20 -0400

    Documentar

--- src/api/user/user.js
+++ src/api/user/user.js
@@ -2,6 +2,13 @@ import axios from 'axios';
 
 const endpoint = "/user/";
 
+/**
+ * Editar el nombre de un usuario
+ * @param {String} user Objeto con la información del usuario
+ * @param {Function} setUser almacenar el usuario en la variable global
+ * @param {String} newName nombre a setear
+ * @returns String success o error
+ */
 const putUser = async (user, setUser, newName) => {
     const userInfo = {
         name: newName,

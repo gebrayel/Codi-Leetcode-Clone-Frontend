cc9eceeaedc5d8dce5738a3772ee0eb8edcf0f35
samuelmarina <samuellmarina@gmail.com>
2021-05-28 18:46:03 -0400

    Incluir temporalmente code editor en profile screen

--- src/screens/Profile/ProfileScreen.js
+++ src/screens/Profile/ProfileScreen.js
@@ -1,28 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Chart from "../../components/Chart/Chart"
+import CodeEditor from "../../components/CodeEditor/CodeEditor";
 
 export default function ProfileScreen({ x, ...props }) {
     const classes = useStyles(props);
+    const [js, setJs] = useState("");
 
-    return (
-        <div className={classes.doughnut_container}>
-            <Chart   
-                labels={['Faltante','Facil', 'Medio', 'Dificil']} 
-                data={[1, 1, 0, 0]} 
-                colors={['#FFCD56', '#FF9F40', '#36A2EB', '#FF6384']}
-                font_color="white"
-                type="Doughnut"
+    return (    
+        <div className={classes.container}>
+            <CodeEditor 
+                onChange={setJs}
+                value={js}
+                language='text/x-java'
             />
         </div>
+
     );
 }
 
 const useStyles = makeStyles((theme) => ({
-    doughnut_container: {
-        marginTop:"10vh",
-        height:400,
-        width:400
+    container: {
+        margin: 100
     },
 
 }));

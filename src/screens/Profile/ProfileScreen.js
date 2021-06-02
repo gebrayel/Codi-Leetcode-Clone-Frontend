3cd519936887c945215347744ca020c9c2573f4a
gebrayel <gebrayel.inatti@correo.unimet.edu.ve>
2021-06-02 05:12:12 -0400

    pic added at profile page

--- src/screens/Profile/ProfileScreen.js
+++ src/screens/Profile/ProfileScreen.js
@@ -1,28 +1,43 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Chart from "../../components/Chart/Chart"
+import Chart from "../../components/Chart/Chart";
 
 export default function ProfileScreen({ x, ...props }) {
-    const classes = useStyles(props);
+  const classes = useStyles(props);
 
+  const image = () => {
+    let x = localStorage.getItem("user");
+    x = JSON.parse(x);
     return (
-        <div className={classes.doughnut_container}>
-            <Chart   
-                labels={['Faltante','Facil', 'Medio', 'Dificil']} 
-                data={[1, 1, 0, 0]} 
-                colors={['#FFCD56', '#FF9F40', '#36A2EB', '#FF6384']}
-                font_color="white"
-                type="Doughnut"
-            />
-        </div>
+      <img
+        src={x["pic_url"]}
+        alt="Girl in a jacket"
+        width="250rem"
+        height="250rem"
+        style={{ borderRadius: "50%" }}
+      />
     );
+  };
+  return (
+    <div style={{ marginTop: "5rem" }}>
+      {image()}
+      <div className={classes.doughnut_container}>
+        <Chart
+          labels={["Faltante", "Facil", "Medio", "Dificil"]}
+          data={[1, 1, 0, 0]}
+          colors={["#FFCD56", "#FF9F40", "#36A2EB", "#FF6384"]}
+          font_color="white"
+          type="Doughnut"
+        />
+      </div>
+    </div>
+  );
 }
 
 const useStyles = makeStyles((theme) => ({
-    doughnut_container: {
-        marginTop:"10vh",
-        height:400,
-        width:400
-    },
-
+  doughnut_container: {
+    marginTop: "10vh",
+    height: 400,
+    width: 400,
+  },
 }));

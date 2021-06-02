da59dd568f73aa0cbb8fd0979d670af9057faae7
gebrayel <gebrayel.inatti@correo.unimet.edu.ve>
2021-06-02 06:55:22 -0400

    editable field done

--- src/screens/Profile/ProfileScreen.js
+++ src/screens/Profile/ProfileScreen.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Chart from "../../components/Chart/Chart";
+import { RefreshSharp } from "@material-ui/icons";
 
 export default function ProfileScreen({ x, ...props }) {
+  let locvalue = localStorage.getItem("user");
+  locvalue = JSON.parse(locvalue);
   const classes = useStyles(props);
-
+  let [text, setText] = useState({
+    value: locvalue["name"],
+    editMode: false,
+    modif: "",
+  });
   const image = () => {
-    let x = localStorage.getItem("user");
-    x = JSON.parse(x);
     return (
       <img
-        src={x["pic_url"]}
+        src={locvalue["pic_url"]}
         alt="Girl in a jacket"
         width="250rem"
         height="250rem"
@@ -18,9 +23,69 @@ export default function ProfileScreen({ x, ...props }) {
       />
     );
   };
+  const changeEditMode = () => {
+    // setText({ ...text, editMode: !text.editMode });
+  };
+  const updateValue = () => {
+    // setText({
+    //   ...text,
+    //   editMode: false,
+    // });
+  };
+  const textEdition = () => {
+    return (
+      <div>
+        <input
+          type="text"
+          onChange={(e) => {
+            console.log(e.target.value);
+            console.log(text.value);
+            setText({ ...text, modif: e.target.value });
+            console.log(text.value);
+            console.log(text.modif);
+          }}
+          id="text"
+          defaultValue={text.value}
+        />
+        <button
+          onClick={() => {
+            setText({ ...text, editMode: !text.editMode, modif: "" });
+            console.log(text.value);
+          }}
+        >
+          X
+        </button>
+        <button
+          onClick={() => {
+            let a = text.modif;
+            setText({ ...text, modif: "", value: a, editMode: false });
+            console.log(text.value);
+          }}
+        >
+          Ok
+        </button>
+      </div>
+    );
+  };
+  const textView = () => {
+    console.log(text.value);
+    return (
+      <div>
+        <div>{text.value}</div>
+        <button
+          onClick={() => {
+            setText({ ...text, editMode: !text.editMode });
+          }}
+        >
+          edit
+        </button>
+      </div>
+    );
+  };
   return (
     <div style={{ marginTop: "5rem" }}>
       {image()}
+      {text.editMode ? textEdition() : textView()}
       <div className={classes.doughnut_container}>
         <Chart
           labels={["Faltante", "Facil", "Medio", "Dificil"]}

a2a2b4ae60008097cd1403337c21e9d6dc1df3cf
rommel sanzonetti <rommel.sanzonetti@gmail.com>
2021-06-08 14:17:54 -0400

    correcciones ProblemTab1

--- src/components/ProblemTabs/ProblemTab1/ProblemTab1.js
+++ src/components/ProblemTabs/ProblemTab1/ProblemTab1.js
@@ -46,87 +46,83 @@ export default function ProblemTab1({problemInfo,handleProblemInfo,code,setCode}
 
   return (
         <div className={classes.root}>
-      <div>
-        <div id="TextFieldBox" >
-          <ThemeProvider theme={theme}>
+          <div>
+            <div id="TextFieldBox" >
+              <ThemeProvider theme={theme}>
+                  <TextField
+                    color="white"
+                    label="Titulo"
+                    id="TitleField"
+                    className={classes.textField}
+                    name="name"
+                    onChange={onChange}
+                    value={problemInfo.name}
+                    />
+                  <TextField
+                    id="SelectField"
+                    select
+                    label="Dificultad"
+                    className={classes.textField}
+                    onChange={onChange}
+                    name="difficulty"
+                    value={problemInfo.difficulty}
+                    >
+                    {difficulties.map((option) => (
+                      <MenuItem key={option.value} value={option.value}>
+                        {option.label}
+                      </MenuItem>
+                    ))}
+                  </TextField>
+                </ThemeProvider>
+                
+            </div>
 
-              <TextField
-                color="white"
-                label="Titulo"
-                id="TitleField"
-                className={classes.textField}
-                name="name"
+            <div id="TextAreaLgBox">
+              <div id="InputLabelDescriptionBox" >
+                <InputLabel id="InputLabelDescription" style={{color:"white",marginBottom:"1rem",textAlign:"start"}} htmlFor="description">Descripcion</InputLabel>
+              </div>
+              <TextareaAutosize
+                style={{width:"85%"}}
+                id="TextAreaLg"
+                className={classes.TextareaAutosizeLg}
+                aria-label="Problem Description" 
+                rowsMin={8} 
+                placeholder="Descripcion del Problema" 
+                name="description"
                 onChange={onChange}
-                value={problemInfo.name}
+                value={problemInfo.description}
+                />
+            </div>
 
+            <div style={{marginLeft: "auto",marginRight: "auto",width:"82%"}} id="TextAreaBox">
+              <div id="InputLabelSolutionBox" style={{width:"200px"}}>
+                <InputLabel id="InputLabelSolution" style={{color:"white",textAlign:"start"}} htmlFor="solution">Solucion</InputLabel>
+              </div>
+              <TextareaAutosize
+              className="TextArea"
+              aria-label="Problem Solution" 
+              rowsMin={8} 
+              placeholder="Solucion del Problema" 
+              helperText="Solucion"
+              name="solution"
+              onChange={onChange}
+              value={problemInfo.solution}
+              autoComplete="false"
+              />
+              <div id="CodeEditorBox">
+                <InputLabel id="InputLabelSolutionCode" style={{color:"white",marginBottom:"1rem",textAlign:"start"}} htmlFor="solutionCode">Codigo Solucion</InputLabel>
+                <CodeEditor
+                  id="CodeEditor"
+                  className="CodeEditorBox"
+                  name='solutionCode'
+                  value={code}
+                  language={languageCode}
+                  onChange={setCode}
                 />
-              <TextField
-                id="SelectField"
-                select
-                label="Dificultad"
-                className={classes.textField}
-                onChange={onChange}
-                name="difficulty"
-                value={problemInfo.difficulty}
-                >
-                {difficulties.map((option) => (
-                  <MenuItem key={option.value} value={option.value}>
-                    {option.label}
-                  </MenuItem>
-                ))}
-              </TextField>
-            </ThemeProvider>
-            
-        </div>
-        <div id="TextAreaLgBox">
-          <div id="InputLabelDescriptionBox" >
-          <InputLabel id="InputLabelDescription" style={{color:"white",marginBottom:"1rem",textAlign:"start"}} htmlFor="description">Descripcion</InputLabel>
+              </div>
+            </div>
           </div>
-          <TextareaAutosize
-            style={{width:"85%"}}
-            id="TextAreaLg"
-            className={classes.TextareaAutosizeLg}
-            aria-label="Problem Description" 
-            rowsMin={8} 
-            placeholder="Descripcion del Problema" 
-            name="description"
-            onChange={onChange}
-            value={problemInfo.description}
-            />
-        </div>
-        <div style={{marginLeft: "auto",marginRight: "auto",width:"82%"}} id="TextAreaBox">
-          
-          <div id="InputLabelSolutionBox" style={{width:"200px"}}>
-            <InputLabel id="InputLabelSolution" style={{color:"white",textAlign:"start"}} htmlFor="solution">Solucion</InputLabel>
-          </div>
-        
-          <TextareaAutosize
-          
-          className="TextArea"
-          aria-label="Problem Solution" 
-          rowsMin={8} 
-          placeholder="Solucion del Problema" 
-          helperText="Solucion"
-          name="solution"
-          onChange={onChange}
-          value={problemInfo.solution}
-          autoComplete="false"
-          />
-          <div id="CodeEditorBox">
-            <InputLabel id="InputLabelSolutionCode" style={{color:"white",marginBottom:"1rem",textAlign:"start"}} htmlFor="solutionCode">Codigo Solucion</InputLabel>
-        
-              <CodeEditor
-                id="CodeEditor"
-                className="CodeEditorBox"
-                name='solutionCode'
-                value={code}
-                language={languageCode}
-                onChange={setCode}
-                />
-          </div>
-        </div>
       </div>
-    </div>
     
 );}
 

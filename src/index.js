96483bcc6616a8cc83df48e43e09f308d3c61c14
andresarl16 <andrucho01@gmail.com>
2021-06-09 00:51:04 -0400

    Little changes

--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './styles/styles.scss';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./styles/styles.scss";
+import * as serviceWorker from "./service-worker";
+import { app } from "./config/firebase/firebase-config";
 
-import { app } from './config/firebase/firebase-config';
-
-import axios from 'axios';
-import Navbar from './components/Navbar/Navbar';
-import settings from './config/settings/settings';
+import axios from "axios";
+import Navbar from "./components/Navbar/Navbar";
+import settings from "./config/settings/settings";
 
 axios.defaults.baseURL = settings.apiUrl;
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorker.register();

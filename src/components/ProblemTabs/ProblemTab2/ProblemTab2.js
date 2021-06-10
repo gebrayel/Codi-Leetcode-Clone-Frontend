0a895cecc81e988932e908d1684348195e7111da
rommel sanzonetti <rommel.sanzonetti@gmail.com>
2021-06-10 02:01:03 -0400

    ProblemTab2 Component ready for QA

--- src/components/ProblemTabs/ProblemTab2/ProblemTab2.js
+++ src/components/ProblemTabs/ProblemTab2/ProblemTab2.js
@@ -1,20 +1,18 @@
-import React,{useState} from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import "../../../styles/screens/ProblemFormScreen/ProblemTabs/ProblemTab2/ProblemTab2.scss";
 import {  TextField,
-          TextareaAutosize,
           MenuItem } from '@material-ui/core';
 import CodeEditor from "../../CodeEditor/CodeEditor";
-import {createMuiTheme, ThemeProvider} from '@material-ui/core/styles';
 import InputLabel from '@material-ui/core/InputLabel';
-
+import colors from "../../../config/colors/colors"
 export default function ProblemTab2({problemInfo,handleProblemInfo,template,setTemplate}) {
   const classes = useStyles();
   // languages: python text/x-java
   var languageCode="text/x-java"
   const languages = [
     {
-      value: 'text/x-java"',
+      value: 'text/x-java',
       label: 'Java',
     },
     {
@@ -22,21 +20,17 @@ export default function ProblemTab2({problemInfo,handleProblemInfo,template,setT
       label: 'Python',
     },
   ];
-  
-  
   const onChange = e =>{
     if(e.target==undefined) return
     handleProblemInfo({
         ...problemInfo,
         [e.target.name]:e.target.value
     })
+    console.log(template)
   }
-
   return (
         <div className={classes.root}>
-      
           <div id="TextFieldBox__Tab2" >
-            
                 <TextField
                   id="SelectField__Tab2"
                   select
@@ -52,7 +46,6 @@ export default function ProblemTab2({problemInfo,handleProblemInfo,template,setT
                     </MenuItem>
                   ))}
                 </TextField>
-              
           </div>
           <div id="CodeEditorBox__Tab2">
             <InputLabel id="TemplateLabel__Tab2" style={{color:"white",marginBottom:"1rem"}} htmlFor="template">Template</InputLabel>
@@ -61,35 +54,45 @@ export default function ProblemTab2({problemInfo,handleProblemInfo,template,setT
               className="CodeEditorBox"
               name='solutionCode'
               value={template}
-              language={languageCode}
+              language={problemInfo.language}
               onChange={setTemplate}
               />
           </div>
-        </div>
-      
+        </div>   
 );}
 
 const useStyles = makeStyles((theme) => ({
-root: {
-  width:"100%",
-  color:"white"
-},
-textField: {
-  color:"white",
-},
-whiteTheme:{
-
-        "& .MuiInputLabel-formControl":{
-            color:"white"
-        }
-          
-      },
-whiteThemeIconSelect:{
+  root: {
+    width:"100%",
+    color:"white"
+  },
+  textField: {
+    color:"white",
+  },
+  whiteTheme:{
 
   "& .MuiSelect-icon":{
-    color:"white !important"
-  },
-  
-}
+    color:"white"
+    },
+  "& .MuiInputLabel-formControl":{
+      color:"white"
+    },
+    "& .MuiInputBase-root ": {
+      color: colors.white,
       
+    },
+    "& .MuiInputBase-input .MuiInput-input": {
+      color: colors.white,
+    },
+    "& .MuiInput-underline:before": {
+      borderColor: colors.white,
+      borderWidth: "0.2rem",
+      borderBottom: "0.2rem solid white",
+    },
+    "& .MuiInput-underline:after": {
+      borderColor: colors.white,
+      borderWidth: "0.2rem",
+      borderBottom: "0.2rem solid white",
+    },
+  },  
 }));

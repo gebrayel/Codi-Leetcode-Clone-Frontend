99a1d18f8f214eaacaa518b0fe91d0f597808b57
rommel sanzonetti <rommel.sanzonetti@gmail.com>
2021-06-10 20:54:27 -0400

    correcciones Tab1 y Tab2 listo

--- src/screens/ProblemForm/ProblemFormScreen.js
+++ src/screens/ProblemForm/ProblemFormScreen.js
@@ -1,21 +1,21 @@
 import React, {useState} from 'react';
 import Button from '@material-ui/core/Button';
-import { makeStyles, withStyles } from '@material-ui/core/styles';
 import StepperC from "../../components/StepperC/StepperC";
 import "../../styles/screens/ProblemFormScreen/ProblemFormScreen.scss";
 
 const ProblemFormScreen = () => {
     const [activeStep, setActiveStep] = useState(0);
     const [code,setCode]=useState("");
-    const [template,setTemplate]=useState("");
+    const [javaTemplate,setJavaTemplate]=useState("");
+    const [pythonTemplate,setPythonTemplate]=useState("");
     const [problemInfo,handleProblemInfo]=useState({
         name:'',
         difficulty:'',
         description:'',
         solution:'',
-        solutionCode:'',
-        language:'',
-        templateCode:''
+        solutionCode:{},
+        language:{},
+        templateCode:{}
 
     })
     const handleNextStepper = () => {
@@ -26,21 +26,42 @@ const ProblemFormScreen = () => {
           
         let validateCases=""
         if(activeStep===0){
-            if(problemInfo.name.trim().replace("\n","").length<3) validateCases=validateCases+"El titulo del problema debe tener al menos 3 caracteres.\n"
-            if(problemInfo.difficulty.trim().replace("\n","")==="") validateCases=validateCases+"Debes seleccionar una dificultad para el problema.\n"
-            if(problemInfo.description.trim().replace("\n","").length<8) validateCases=validateCases+"Debes agregar una descripcion para el problema de al menos 8 caracteres.\n"
-            if(problemInfo.solution.trim().replace("\n","")==="") validateCases=validateCases+"Debes agregar una solucion para el problema.\n"
-            if(code.trim().replace("\n","")==="") validateCases=validateCases+"Debes agregar un codigo solucion al problema.\n"
+            if(problemInfo.name.trim().replace("\n","").length<3) {
+                validateCases=validateCases+"El titulo del problema debe tener al menos 3 caracteres.\n"
+            }
+            if(problemInfo.difficulty.trim().replace("\n","")===""){ 
+                validateCases=validateCases+"Debes seleccionar una dificultad para el problema.\n"
+            }
+            if(problemInfo.description.trim().replace("\n","").length<8) {
+                validateCases=validateCases+"Debes agregar una descripcion para el problema de al menos 8 caracteres.\n"
+            }
+            if(problemInfo.solution.trim().replace("\n","")===""){ 
+                validateCases=validateCases+"Debes agregar una solucion para el problema.\n"
+            }
+            if(code.trim().replace("\n","")==="") {
+                validateCases=validateCases+"Debes agregar un codigo solucion al problema.\n"
+            }
         }else if(activeStep===1){
-            if(problemInfo.language.trim().replace("\n","")==="") validateCases=validateCases+"Debes asignar un lenguaje al problema.\n"
-            if(template.trim().replace("\n","")==="") validateCases=validateCases+"Debes agregar un template al problema.\n"
+            
+            if(
+                problemInfo.templateCode.javaTemplate===undefined 
+                || problemInfo.templateCode.javaTemplate.trim().replace("\n","")===""
+                ){ 
+                validateCases=validateCases+"Debes agregar un template al problema en codigo Java.\n"
+            }
+            if(
+                problemInfo.templateCode.pythonTemplate===undefined 
+                || problemInfo.templateCode.pythonTemplate.trim().replace("\n","")===""
+                ){ 
+                validateCases=validateCases+"Debes agregar un template al problema en codigo Python.\n"
+            }
+
         }
         if(validateCases.trim().replace("\n","")!=="") {alert(validateCases); return;}
         setActiveStep((prevActiveStep) => prevActiveStep + 1);
         console.log(problemInfo);
-        // console.log(activeStep);
-        // console.log(code);
-        console.log(template);
+        console.log(javaTemplate);
+        console.log(pythonTemplate)
         }
   };
   const handleBackStepper = () => {
@@ -61,8 +82,10 @@ const ProblemFormScreen = () => {
                 handleProblemInfo={handleProblemInfo}
                 code={code}
                 setCode={setCode}
-                template={template}
-                setTemplate={setTemplate}
+                javaTemplate={javaTemplate}
+                setJavaTemplate={setJavaTemplate}
+                pythonTemplate={pythonTemplate}
+                setPythonTemplate={setPythonTemplate}
             />
             <div id="buttonBox">
                 {activeStep !== 0 ? 

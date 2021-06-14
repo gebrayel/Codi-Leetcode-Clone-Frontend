650e3011ae2b8e88a11bca010bc61bc246e3a6a9
pstefy <stefani.perez@correo.unimet.edu.ve>
2021-06-14 09:06:59 -0400

    Merge 2

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import React, { useState, useEffect } from "react";
-=======
-import React, { useState, useEffect, useContext } from "react";
->>>>>>> develop
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";

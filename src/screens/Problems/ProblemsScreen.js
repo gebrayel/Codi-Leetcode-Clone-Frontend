03f61d2c71444a8ef40dcbaeb608ad77d1332448
rommel sanzonetti <rommel.sanzonetti@gmail.com>
2021-06-16 17:52:44 -0400

    add edit icon to every single problem and redirect to the IDE Screen

--- src/screens/Problems/ProblemsScreen.js
+++ src/screens/Problems/ProblemsScreen.js
@@ -6,50 +6,54 @@ import ProblemList from "../../components/ProblemList/ProblemList";
 import problemAPI from "../../api/problems/problems";
 import RobotLoader from "../../components/RobotLoader/RobotLoader";
 import useQuery from "../../hooks/useQuery/useQuery";
+import EditIcon from "@material-ui/icons/Edit";
+
 const ProblemsScreen = () => {
-    const query = useQuery();
-    const [problems, setProblems] = useState([]);
-    const user = JSON.parse(localStorage.getItem('user'));
-    const { isLoading, setIsLoading } = useContext(AppContext);
+  const query = useQuery();
+  const [problems, setProblems] = useState([]);
+  const user = JSON.parse(localStorage.getItem("user"));
+  const { isLoading, setIsLoading } = useContext(AppContext);
 
-    const spanishDifficulty = {
-        easy: "Fácil",
-        medium: "Intermedio",
-        hard: "Difícil",
-    };
+  const spanishDifficulty = {
+    easy: "Fácil",
+    medium: "Intermedio",
+    hard: "Difícil",
+  };
 
-    useEffect(() => {
-        const difficulty = query.get("difficulty");
-        const getProblems = async (difficulty, user) => {
-            setIsLoading(true);
-            const probs = await problemAPI.getProblemsByDifficulty(difficulty, user);
-            let arrayProbs = probs.map((prob, index) => {
-                return { ...prob, 
-                    color: index % 2 === 0 ? colors.evenRow : colors.oddRow,
-                    difficulty: spanishDifficulty[prob.difficulty] 
-                }
-            })
-            setProblems(arrayProbs);
-            setIsLoading(false);
+  useEffect(() => {
+    const difficulty = query.get("difficulty");
+    const getProblems = async (difficulty, user) => {
+      setIsLoading(true);
+      const probs = await problemAPI.getProblemsByDifficulty(difficulty, user);
+      let arrayProbs = probs.map((prob, index) => {
+        return {
+          ...prob,
+          color: index % 2 === 0 ? colors.evenRow : colors.oddRow,
+          difficulty: spanishDifficulty[prob.difficulty],
+          edit: (
+            <a id="editProblemLink" href={"/IDE?problemId=" + prob.problem_id}>
+              <EditIcon />
+            </a>
+          ),
         };
-        getProblems(difficulty, user);
-    }, []);
-
-    return (
-        <div className="ProblemScreenContainer">
-        { isLoading ? 
-        (
-            <RobotLoader />
-        ) :
-        (
-            <div className="ProblemListContainer">
-                <h1 className="ProblemScreenTitle"> Problemas </h1>
-                <ProblemList rows={problems} />
-            </div>
-        )}
-            
+      });
+      setProblems(arrayProbs);
+      setIsLoading(false);
+    };
+    getProblems(difficulty, user);
+  }, []);
+  return (
+    <div className="ProblemScreenContainer">
+      {isLoading ? (
+        <RobotLoader />
+      ) : (
+        <div className="ProblemListContainer">
+          <h1 className="ProblemScreenTitle"> Problemas </h1>
+          <ProblemList rows={problems} />
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default ProblemsScreen;

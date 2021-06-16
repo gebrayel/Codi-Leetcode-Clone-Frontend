c583bc37ab4226eda504f4acef46072c2a672085
gebrayel <gebrayel.inatti@correo.unimet.edu.ve>
2021-06-16 10:32:05 -0400

    setting states for console

--- src/components/CodeConsole/CodeConsole.js
+++ src/components/CodeConsole/CodeConsole.js
@@ -1,9 +1,24 @@
 import React, { useContext, useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
 
-export default function CustomInput() {
+export default function CustomInput(input, output, esperado, isLoading) {
+  const classes = useStyles();
+
+  const consoleNew = () => {
+    return (
+      <>
+        <div>
+          clic run when u feel ready remind ur smart...i guess dont forget the ;
+          att your javaby
+        </div>
+      </>
+    );
+  };
+  const consoleUsed = () => {};
+  const showLoader = () => {};
   return (
-    <>
-      <div>Dick</div>
-    </>
+    <>{isLoading ? showLoader : output == "" ? consoleNew : consoleUsed}</>
   );
 }
+
+const useStyles = makeStyles((theme) => ({}));

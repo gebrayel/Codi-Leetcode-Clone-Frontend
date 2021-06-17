400b5e8a8b85b3bf6e5d72561643a92a0640b291
samuelmarina <samuellmarina@gmail.com>
2021-06-17 13:26:28 -0400

    Agregar constante

--- src/helpers/constants/constants.js
+++ src/helpers/constants/constants.js
@@ -3,7 +3,21 @@ const k = {
     cardNumberLength: 16,
     cardNameLength: 4,
     cardExpiryLength: 4,
-    cardCvcLength: 3
+    cardCvcLength: 3,
+    difficultiesDropdown: [
+    {
+      value: "easy",
+      label: "Facil",
+    },
+    {
+      value: "medium",
+      label: "Intermedio",
+    },
+    {
+      value: "hard",
+      label: "Dificil",
+    },
+  ]
 };
 
 export default k;

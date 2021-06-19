ebb42186f1b6013ef0cc9642f419a77a357475db
rommel sanzonetti <rommel.sanzonetti@gmail.com>
2021-06-18 22:58:00 -0400

    probar updateProblem function in develop mode

--- src/config/settings/settings.js
+++ src/config/settings/settings.js
@@ -1,15 +1,15 @@
 const settings = {
-    develop: {
-        apiUrl: "http://127.0.0.1:3000"
-    },
-    production: {
-        apiUrl: "https://codi-backends.herokuapp.com/"
-    }
-}
+  develop: {
+    apiUrl: "http://127.0.0.1:3000",
+  },
+  production: {
+    apiUrl: "https://codi-backends.herokuapp.com/",
+  },
+};
 
 const getCurrentSettings = (flag) => {
-    if(flag) return settings.develop;
-    return settings.production;
-}
+  if (flag) return settings.develop;
+  return settings.production;
+};
 
 export default getCurrentSettings(false);

dbdad9e94a27621f6c849ee36c16f3c316d63926
andresarl16 <andrucho01@gmail.com>
2021-06-20 20:50:27 -0400

    Modal Styles completed

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -5,6 +5,7 @@ import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
+import { makeStyles } from "@material-ui/core/styles";
 
 const Modal = ({
     title,
@@ -25,6 +26,7 @@ const Modal = ({
      *      modalBtnAcept Establece el texto con primary desing (afirmacion==>Aceptar) del boton del Componente Modal.
      *      modalBtnCancel Establece el texto con secondary desing (negacion==>Cancelar) del boton del Componente Modal.
      */
+    const classes = useStyles();
 
     return (
         <div style={{ width: "100%", justifyContent: "flex-start" }}>
@@ -34,32 +36,60 @@ const Modal = ({
                 aria-labelledby="alert-dialog-title"
                 aria-describedby="alert-dialog-description"
             >
-                <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
-                <DialogContent>
-                    <DialogContentText id="alert-dialog-description">
-                        {description}
-                    </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    {!singleButton ? (
-                        <Button
-                            onClick={() => {
-                                toggleModal();
-                                passedRedFunction();
-                            }}
-                            color="secondary"
-                            autoFocus
+                <div className={classes.root}>
+                    <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText
+                            id="alert-dialog-description"
+                            className={classes.whiteTheme}
                         >
-                            {cancelText}
+                            {description}
+                        </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                        {!singleButton ? (
+                            <Button
+                                onClick={() => {
+                                    toggleModal();
+                                    passedRedFunction();
+                                }}
+                                color="secondary"
+                                autoFocus
+                            >
+                                {cancelText}
+                            </Button>
+                        ) : null}
+                        <Button onClick={passedBlueFunction} color="primary">
+                            {acceptText}
                         </Button>
-                    ) : null}
-                    <Button onClick={passedBlueFunction} color="primary">
-                        {acceptText}
-                    </Button>
-                </DialogActions>
+                    </DialogActions>
+                </div>
             </Dialog>
         </div>
     );
 };
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        backgroundColor: "#60626C",
+        color: "white",
+        "& .MuiButton-textPrimary": {
+            color: "#8393FF",
+            fontSize: "15px",
+            fontWeight: 500,
+        },
+        "& .MuiButton-textSecondary": {
+            color: "#E3516E",
+            fontSize: "15.5px",
+            fontWeight: 500,
+        },
+        "& .MuiTypography-body1": {
+            fontSize: "1.2rem",
+        },
+    },
+    whiteTheme: {
+        color: "white",
+    },
+}));
 
 export default Modal;

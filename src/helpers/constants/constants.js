e8f9320276267a1e2e016591a3d3de197bb72d16
samuelmarina <samuellmarina@gmail.com>
2021-06-22 16:15:52 -0400

    Agregar constante

--- src/helpers/constants/constants.js
+++ src/helpers/constants/constants.js
@@ -1,38 +1,42 @@
 const k = {
-    //Card details
-    cardNumberLength: 16,
-    cardNameLength: 4,
-    cardExpiryLength: 4,
-    cardCvcLength: 3,
-    difficultiesDropdown: [
-      {
-        value: "easy",
-        label: "Facil",
-      },
-      {
-        value: "medium",
-        label: "Intermedio",
-      },
-      {
-        value: "hard",
-        label: "Dificil",
-      },
-    ],
-    languagesDropdown: [
-      {
-        value: "text/x-java",
-        label: "Java",
-      },
-      {
-        value: "python",
-        label: "Python",
-      },
+  //Card details
+  cardNumberLength: 16,
+  cardNameLength: 4,
+  cardExpiryLength: 4,
+  cardCvcLength: 3,
+  difficultiesDropdown: [
+    {
+      value: "easy",
+      label: "Facil",
+    },
+    {
+      value: "medium",
+      label: "Intermedio",
+    },
+    {
+      value: "hard",
+      label: "Dificil",
+    },
   ],
-    spanishDifficulty: {
-      easy: "Fácil",
-      medium: "Intermedio",
-      hard: "Difícil",
-    }
+  languagesDropdown: [
+    {
+      value: "text/x-java",
+      label: "Java",
+    },
+    {
+      value: "python",
+      label: "Python",
+    },
+  ],
+  codeLanguages: {
+    "Java": "text/x-java",
+    "Python": "python"
+  },
+  spanishDifficulty: {
+    easy: "Fácil",
+    medium: "Intermedio",
+    hard: "Difícil",
+  }
 };
 
 export default k;

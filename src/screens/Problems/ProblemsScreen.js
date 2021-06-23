5d977533803a4d770d5bbebdcd046859b3934d2a
rommel sanzonetti <rommel.sanzonetti@gmail.com>
2021-06-22 23:19:52 -0400

    add create Problem button

--- src/screens/Problems/ProblemsScreen.js
+++ src/screens/Problems/ProblemsScreen.js
@@ -1,5 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
-
+import { makeStyles } from "@material-ui/core/styles";
+import { Button } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
 import AppContext from "../../helpers/context/context";
 import colors from "../../config/colors/colors";
 import ProblemList from "../../components/ProblemList/ProblemList";
@@ -9,11 +11,12 @@ import useQuery from "../../hooks/useQuery/useQuery";
 import EditIcon from "@material-ui/icons/Edit";
 
 const ProblemsScreen = () => {
+  const classes = useStyles();
   const query = useQuery();
   const [problems, setProblems] = useState([]);
   const user = JSON.parse(localStorage.getItem("user"));
   const { isLoading, setIsLoading } = useContext(AppContext);
-
+  const history = useHistory();
   const spanishDifficulty = {
     easy: "Fácil",
     medium: "Intermedio",
@@ -59,8 +62,24 @@ const ProblemsScreen = () => {
         <RobotLoader />
       ) : (
         <div className="ProblemListContainer">
-          <h1 className="ProblemScreenTitle"> Problemas </h1>
-          <ProblemList rows={problems} />
+          <div>
+            <div className="BoxTitle__ProblemScreen">
+              <h1 className="ProblemScreenTitle"> Problemas </h1>
+            </div>
+            <div className="buttonBox__ProblemScreen">
+              <Button
+                className={classes.buttonAll}
+                onClick={() => history.push("/problemForm")}
+                variant="contained"
+                color="primary"
+              >
+                /*Crear Problema*/
+              </Button>
+            </div>
+          </div>
+          <div className="ProblemListContainerBox">
+            <ProblemList rows={problems} />
+          </div>
         </div>
       )}
     </div>
@@ -68,3 +87,38 @@ const ProblemsScreen = () => {
 };
 
 export default ProblemsScreen;
+
+const useStyles = makeStyles((theme) => ({
+  buttonAll: {
+    background: "rgba(126, 132, 167, 0.63)",
+    color: "#ECECEC",
+    fontFamily: "Roboto",
+    fontStyle: "normal",
+    fontWeight: "bold",
+    borderRadius: 0,
+    height: "60px",
+    border: "1px solid rgba(99, 98, 98, 0.26)",
+    boxSizing: "border-box",
+    boxShadow: "5px 4px 4px rgba(0, 0, 0, 0.28)",
+    borderRadius: "10px",
+    fontSize: "100%",
+    transition: "background 1s",
+    transition: "color 1s",
+
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "70%",
+    },
+    [theme.breakpoints.down("xs")]: {
+      height: "50px",
+      fontSize: "70%",
+    },
+
+    "&:hover": {
+      transition: "background 1s",
+      transition: "color 1s",
+      background: "#FFFFFF",
+      color: "#474747",
+      cursor: "pointer",
+    },
+  },
+}));

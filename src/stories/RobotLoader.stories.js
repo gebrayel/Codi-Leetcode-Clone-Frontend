d3e4d56464f3d65451680cb9a6799f16702faca9
pstefy <stefani.perez@correo.unimet.edu.ve>
2021-06-27 23:40:46 -0400

    Listo doc RobotLoader

---
+++ src/stories/RobotLoader.stories.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import RobotLoader from "../components/RobotLoader/RobotLoader";
+
+export default {
+    title: "Codi/RobotLoader",
+    component: RobotLoader,
+    argTypes: {
+    },
+};
+
+const Template = () => <RobotLoader />;
+
+export const RobotDeCarga = Template.bind({});

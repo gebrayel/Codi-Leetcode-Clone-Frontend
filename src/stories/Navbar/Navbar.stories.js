371027d1046379d1c08097c9c9eb7ae5aa6d8f6e
gebrayel <gebrayel.inatti@correo.unimet.edu.ve>
2021-06-27 23:17:12 -0400

    navbar init

---
+++ src/stories/Navbar/Navbar.stories.js
@@ -0,0 +1,14 @@
+// import React from "react";
+// import Navbar from "../../components/Navbar/Navbar";
+
+// export default {
+//   title: "Codi/Navbar",
+//   component: Navbar,
+//   argTypes: {},
+// };
+
+// const Template = () => <Navbar />;
+
+// export const Vista_Escritorio = Template.bind({});
+
+// export const Vista_Movil = Template.bind({});

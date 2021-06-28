9ccf8dfebdd85404ba3d1fcf300869c91121eb65
pstefy <stefani.perez@correo.unimet.edu.ve>
2021-06-27 22:49:40 -0400

    Listo doc del CubeLoader

---
+++ src/stories/CubeLoader.stories.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import CubeLoader from "../components/CubeLoader/CubeLoader";
+
+export default {
+    title: "Codi/CubeLoader",
+    component: CubeLoader,
+    argTypes: {
+    },
+};
+
+const Template = () => <CubeLoader />;
+
+export const CuboDeCarga = Template.bind({});
